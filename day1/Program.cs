fc6aa7234269720f2f752d07909401d082abc3c8
perskii <86572700+perskii@users.noreply.github.com>
2021-07-04 20:55:55 +0200

    Add solution

---
+++ day1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Day1Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Day1part1();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n#####################");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Day1part2();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Day1part1()
+        {
+
+            char[] text = File.ReadAllText(@"E:\adventofcode2015\day1part1\Day1Part1\part1day1.txt").ToCharArray();
+            /*Console.WriteLine(text);*/
+
+
+            int floor = 0;
+
+            foreach (char item in text)
+            {
+                if (item == '(')
+                    floor++;
+                else if (item == ')')
+                    floor--;
+            }
+            Console.Write("Santa is coming ... "+floor);
+            
+        }
+
+        public static void Day1part2()
+        {
+            char[] text = File.ReadAllText(@"E:\adventofcode2015\day1part1\Day1Part1\part1day1.txt").ToCharArray();
+            int stop=0;
+            /*Console.WriteLine(text);*/
+
+            int floor = 0;
+
+            for (int i =0; i< text.Length; i++)
+            {
+                if (text[i] == '(')
+                    floor++;
+                else if (text[i] == ')')
+                    floor--;
+                if (floor == -1) { 
+                    stop = i +1;
+                    //1769 floor 0
+                    //1770 floor -1
+                    break;
+                }
+            }
+            Console.Write("Back to the basement ... "+stop);
+
+        }
+    }
+}
